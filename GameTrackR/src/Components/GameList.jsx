bd8fbc1ef1b75663de479b441ab236614f861866
Bryan Tsanga <bryan-t@hotmail.fr>
2023-05-11 00:08:11 +0200

    worked on the gameList component that is almost finished

--- GameTrackR/src/Components/GameList.jsx
+++ GameTrackR/src/Components/GameList.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import axios from "axios";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function GameList() {
   const [allGames, setAllGames] = useState([]);
@@ -20,22 +21,84 @@ function GameList() {
     getVideoGames();
   }, []);
 
+  //Now let's handle what happens when we reach the bottom of the page :
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
+  const sentinelRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetchElements();
+  }, [currentPage]);
+
+  function fetchElements() {
+    setIsFetching(true);
+
+    axios
+      .get(
+        `https://api.rawg.io/api/games?key=b600c722cedc401fb777d82d17949bec&page=${currentPage}`
+      )
+      .then((response) => {
+        setAllGames((prevElements) => [
+          ...prevElements,
+          ...response.data.results,
+        ]);
+        setIsFetching(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsFetching(false);
+      });
+  }
+
+  function handleScroll() {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  }
+
+  //Now that it's done, let's display the page !
+
   return (
-    <div className="GameList-video-game-page">
+    <div>
       {!allGames ? (
         <div>Loading...</div>
       ) : (
-        allGames.map((elem) => {
-          return (
-            <div
-              key={elem.id}
-              className="GameList-video-game"
-              style={{ backgroundImage: `url(${elem.background_image})` }}
-            >
-              <div className="GameList-video-game-name">{elem.name}</div>
-            </div>
-          );
-        })
+        <div className="GameList-video-game-page">
+          <nav className="header">
+            <ul>
+              <li>filter</li>
+              <li>sort</li>
+            </ul>
+          </nav>
+          {allGames.map((elem) => {
+            const url = `/game-list/${elem.id}`;
+            return (
+              <Link
+                key={elem.id}
+                to={url}
+                className="GameList-video-game"
+                style={{ backgroundImage: `url(${elem.background_image})` }}
+              >
+                <div>
+                  <div className="GameList-video-game-name">{elem.name}</div>
+                </div>
+              </Link>
+            );
+          })}
+          {isFetching && <div>Loading...</div>}
+          <div ref={sentinelRef}></div>
+        </div>
       )}
     </div>
   );
