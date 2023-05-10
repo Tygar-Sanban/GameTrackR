f3d447af58aa8e299992de25079adc87d36f139c
Bryan Tsanga <bryan-t@hotmail.fr>
2023-05-10 20:04:45 +0200

    done some work on the gamelist, the homepage (a little bit) and the CSS

--- GameTrackR/src/Components/GameList.jsx
+++ GameTrackR/src/Components/GameList.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 function GameList() {
-  return <div>This is the GameList</div>;
+  const [allGames, setAllGames] = useState([]);
+
+  async function getVideoGames() {
+    try {
+      const response = await axios.get(
+        "https://api.rawg.io/api/games?key=b600c722cedc401fb777d82d17949bec"
+      );
+      setAllGames(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getVideoGames();
+  }, []);
+
+  return (
+    <div className="GameList-video-game-page">
+      {!allGames ? (
+        <div>Loading...</div>
+      ) : (
+        allGames.map((elem) => {
+          return (
+            <div
+              key={elem.id}
+              className="GameList-video-game"
+              style={{ backgroundImage: `url(${elem.background_image})` }}
+            >
+              <div className="GameList-video-game-name">{elem.name}</div>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 }
 
 export default GameList;
--- GameTrackR/src/Components/HomePage.jsx
+++ GameTrackR/src/Components/HomePage.jsx
@@ -1,37 +1,12 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-  const [allGames, setAllGames] = useState([]);
-
-  async function getVideoGames() {
-    try {
-      const response = await axios.get(
-        "https://api.rawg.io/api/games?key=b600c722cedc401fb777d82d17949bec&genres=adventure"
-      );
-      setAllGames(response.data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    getVideoGames();
-  }, []);
-
   return (
-    <div>
-      {!allGames ? (
-        <div>Loading...</div>
-      ) : (
-        allGames.map((elem) => {
-          return (
-            <div key={elem.id}>
-              {elem.name} <img src={elem.background_image} alt={elem.name} />
-            </div>
-          );
-        })
-      )}
+    <div className="HomePage-GameList-button">
+      <Link to="/game-list">
+        <h1>Game list</h1>
+      </Link>{" "}
     </div>
   );
 }
