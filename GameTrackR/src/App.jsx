e12e252a4a228e0e9360b8be522f1fb273fd0b1d
Bryan Tsanga <bryan-t@hotmail.fr>
2023-05-10 18:04:43 +0200

    Project ready to begin

--- GameTrackR/src/App.jsx
+++ GameTrackR/src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import GameDetails from "./Components/GameDetails";
+import GameList from "./Components/GameList";
 import HomePage from "./Components/HomePage";
+import { Routes, Route } from "react-router-dom";
+import UserProfile from "./Components/UserProfile";
+import Stats from "./Components/Stats";
+import News from "./Components/News";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/game-list" element={<GameList />} />
+        <Route path="/game-details" element={<GameDetails />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
     </>
   );
 }
