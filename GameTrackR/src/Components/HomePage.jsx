67efdbb7f9d1887d98bbfa8412d7b9f5f7e9a85c
Bryan Tsanga <bryan-t@hotmail.fr>
2023-05-11 09:20:55 +0200

    gamelist pretty cool, a little bit of work on the home page

--- GameTrackR/src/Components/HomePage.jsx
+++ GameTrackR/src/Components/HomePage.jsx
@@ -3,11 +3,26 @@ import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div className="HomePage-GameList-button">
-      <Link to="/game-list">
-        <h1>Game list</h1>
-      </Link>{" "}
-    </div>
+    <>
+      <nav className="header">
+        <ul>
+          <li>logo</li>
+          <li>sort</li>
+        </ul>
+      </nav>
+      <container className="HomePage-container">
+        <div className="HomePage-GameList-button">
+          <Link to="/game-list">
+            <h1>Game list</h1>
+          </Link>{" "}
+        </div>
+        <div className="HomePage-UserProfile-button">
+          <Link to="/user-profile">
+            <h1>User Profile</h1>
+          </Link>{" "}
+        </div>
+      </container>
+    </>
   );
 }
 
